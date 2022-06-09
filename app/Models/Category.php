2070b86036cbc11155054138fee6a83872841f77
Khulood Batis <khuloodbatis@gmail.com>
2022-06-09 11:30:04 +0300

    Initial commit

---
+++ app/Models/Category.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
+
+class Category extends Model
+{
+    use HasFactory;
+    use HasRecursiveRelationships;
+
+    
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_id')->with('children');
+    }
+}
